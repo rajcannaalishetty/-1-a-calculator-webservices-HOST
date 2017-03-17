34641cd0496e013984af6ea28ec680d0588758d2
rajca <rajca@RAJCANNA>
2017-03-17 16:54:19 -0500

    Add project files.

---
+++ SeriviceHosting/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriviceHosting
+{
+    public class Class1
+    {
+    }
+}
